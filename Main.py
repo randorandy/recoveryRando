abebbbee5cbe987f272acec813b9a098f6315e17
randorandy <99529287+randorandy@users.noreply.github.com>
2023-06-01 21:11:50 -0700

    Morph at morph fix

--- Main.py
+++ Main.py
@@ -126,7 +126,8 @@ def assumed_fill(game: Game) -> tuple[bool]:
             # but dropping from spaceport can mess that up,
             # so it needs to be checked again.
             #completable, _, _ = solve(game)
-            completable = True
+            #completable = True
+            completable = game.all_locations["Morph Ball"]["item"] == Items.Morph
             if completable:
                 print("Item placements successful.")
             return completable
